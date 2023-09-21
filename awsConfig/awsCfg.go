3a3b2823e531faa8b1d19ed6157d35695fc178cc
Victor Bejas <victorbejas97@gmail.com>
2023-09-21 16:21:46 -0300

    create config with aws

---
+++ awsConfig/awsCfg.go
@@ -0,0 +1,20 @@
+package awsConfig
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+var Ctx context.Context
+var Cfg aws.Config
+var err error
+
+func InitAWS() {
+	Ctx = context.TODO()
+	Cfg, err = config.LoadDefaultConfig(Ctx, config.WithDefaultRegion("us-east-1"))
+	if err != nil {
+		panic("Error to load config .aws/config" + err.Error())
+	}
+}
